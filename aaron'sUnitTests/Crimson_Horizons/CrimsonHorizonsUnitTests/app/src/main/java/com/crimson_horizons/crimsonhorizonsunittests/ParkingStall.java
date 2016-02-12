9fbb817b9ac650760da2b05ddffa8b6c12006117
schuman-aaron <schuman@ualberta.ca>
2016-02-11 21:18:24 -0700

    Added Unit Tests, submitable, may be edited if unsatisfactory

---
+++ aaron'sUnitTests/Crimson_Horizons/CrimsonHorizonsUnitTests/app/src/main/java/com/crimson_horizons/crimsonhorizonsunittests/ParkingStall.java
@@ -0,0 +1,16 @@
+package com.crimson_horizons.crimsonhorizonsunittests;
+
+/**
+ * Created by schuman on 2/5/16.
+ */
+public class ParkingStall {
+    private String availability;
+
+    public String getAvailability() {
+        return availability;
+    }
+
+    public void setAvailability(String input){
+        this.availability = input;
+    }
+}
