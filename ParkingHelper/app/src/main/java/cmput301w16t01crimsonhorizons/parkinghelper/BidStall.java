177c1df99e0172397abd7c3c85606756e3cda85d
kliangualberta <kliang@ualberta.ca>
2016-02-21 19:05:47 -0700

    added new intent for parkinghelper. Organized the use cases into new file called AllUseCases.txt according to Stephen's email

---
+++ ParkingHelper/app/src/main/java/cmput301w16t01crimsonhorizons/parkinghelper/BidStall.java
@@ -0,0 +1,35 @@
+package cmput301w16t01crimsonhorizons.parkinghelper;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
+
+public class BidStall extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bid_stall);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    public void BidStall(View view){
+        Toast.makeText(BidStall.this, "You have made the bid!", Toast.LENGTH_SHORT).show();
+    }
+
+}
--- ParkingHelper/app/src/main/java/cmput301w16t01crimsonhorizons/parkinghelper/Search.java
+++ ParkingHelper/app/src/main/java/cmput301w16t01crimsonhorizons/parkinghelper/Search.java
@@ -1,32 +1,46 @@
 package cmput301w16t01crimsonhorizons.parkinghelper;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class Search extends AppCompatActivity {
-
+    private ListView Result;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        Result = (ListView)findViewById(R.id.ResultLv);
     }
 
+    @Override
+    protected void onStart(){
+        super.onStart();
+        ArrayList<String> TempString = new ArrayList<>();
+        TempString.add("first\n");
+        TempString.add("second\n");
+        Result.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent clickResult = new Intent(view.getContext(), BidStall.class);
+                String stall;
+                stall = Result.getItemAtPosition(position).toString();
+                clickResult.putExtra("stall", stall);
+                clickResult.putExtra("id", position);
+                startActivity(clickResult);
+
+            }
+        });
+        ArrayAdapter<String>adapter = new ArrayAdapter<String>(this,R.layout.own_stalls_with_bids,
+                                                                TempString);
+        Result.setAdapter(adapter);
+    }
     public void clickSearch(View view){
         // TODO: 2/15/2016 Fill in what happens when search is hit again
 
