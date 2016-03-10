1032a122c64cfa67a427d2543d10393bed58877a
kliangualberta <kliang@ualberta.ca>
2016-03-09 18:31:48 -0700

    gitignore file and search

--- ParkingHelper/app/src/main/java/cmput301w16t01crimsonhorizons/parkinghelper/Search.java
+++ ParkingHelper/app/src/main/java/cmput301w16t01crimsonhorizons/parkinghelper/Search.java
@@ -5,12 +5,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
-<<<<<<< HEAD
-=======
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
->>>>>>> 46cde72d9fba7af48a9990401fcc725e9376b3f3
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -23,36 +20,28 @@ import java.util.concurrent.ExecutionException;
  */
 public class Search extends AppCompatActivity implements ViewInterface<Commands> {
     private ListView Result;
-<<<<<<< HEAD
     private ArrayList<Stalls>StallAry = new ArrayList<>();
-=======
     private Button searchBtn;
     private EditText searchBox;
     private ArrayList<Stalls> stalls;
     ArrayAdapter<Stalls> adapter;
-
->>>>>>> 46cde72d9fba7af48a9990401fcc725e9376b3f3
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
         Result = (ListView)findViewById(R.id.ResultLv);
-<<<<<<< HEAD
         String[] GetAvailable= new String[2];
         EditStallSave command = new EditStallSave();
         command.addView(this);
         updateView(command);
-=======
         searchBtn = (Button)findViewById(R.id.SearchBtn);
         searchBox = (EditText)findViewById(R.id.query);
         stalls = new ArrayList<Stalls>();
->>>>>>> 46cde72d9fba7af48a9990401fcc725e9376b3f3
     }
 
     @Override
     protected void onStart(){
         super.onStart();
-<<<<<<< HEAD
         EditStallSave command = new EditStallSave();
         updateView(command);
     }
@@ -76,8 +65,6 @@ public class Search extends AppCompatActivity implements ViewInterface<Commands>
         GetAvailable[0] = "Available";
         GetAvailable[1] = "Status";
         StallAry = model.UpdateStall(GetAvailable);
-=======
->>>>>>> 46cde72d9fba7af48a9990401fcc725e9376b3f3
         Result.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -90,11 +77,34 @@ public class Search extends AppCompatActivity implements ViewInterface<Commands>
 
             }
         });
-<<<<<<< HEAD
-        Result.setAdapter(new AdapterEditStall(this, R.layout.account_stalls, StallAry));
+        searchBtn.setOnClickListener( new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                ElasticSearchCtr.SearchDataBaseTask searchTask =
+                        new ElasticSearchCtr.SearchDataBaseTask();
+                String keyWords = searchBox.getText().toString();
+                searchTask.execute(keyWords);
+
+                try {
+                    stalls = searchTask.get();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
+                }
+
+                adapter.notifyDataSetChanged();
+            }
+        });
+
+        adapter = new ArrayAdapter<Stalls>(this, R.layout.own_stalls_with_bids,
+                stalls);
+        Result.setAdapter(adapter);
     }
+
 }
-=======
+/*=======
 
         searchBtn.setOnClickListener( new View.OnClickListener(){
 
@@ -127,4 +137,4 @@ public class Search extends AppCompatActivity implements ViewInterface<Commands>
     }
 
 }
->>>>>>> 46cde72d9fba7af48a9990401fcc725e9376b3f3
+>>>>>>> 46cde72d9fba7af48a9990401fcc725e9376b3f3*/
