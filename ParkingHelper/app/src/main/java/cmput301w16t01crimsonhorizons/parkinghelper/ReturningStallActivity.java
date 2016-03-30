0fd3da948ba7ecf063ab4fcdeb5edfdb1a02989e
JPeard <jinchen12@hotmail.com>
2016-03-29 18:20:40 -0600

    Now implemented list of items you are lending to someone else

--- ParkingHelper/app/src/main/java/cmput301w16t01crimsonhorizons/parkinghelper/ReturningStallActivity.java
+++ ParkingHelper/app/src/main/java/cmput301w16t01crimsonhorizons/parkinghelper/ReturningStallActivity.java
@@ -1,18 +1,29 @@
 package cmput301w16t01crimsonhorizons.parkinghelper;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageView;
 
 public class ReturningStallActivity extends AppCompatActivity {
+    protected Stalls stall;
+    private Intent intent;
+    private Bitmap thumbnail;
+    private ImageView picture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_returning_stall);
+
+        intent = getIntent();
+        stall = (Stalls)intent.getSerializableExtra("entry");
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -25,6 +36,10 @@ public class ReturningStallActivity extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    public void returnStall(View view){
+
     }
 
 }
