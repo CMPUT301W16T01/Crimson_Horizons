755b5b034e8a7c74f9cbb54c9b0017853b9e4bc6
JPeard <jinchen12@hotmail.com>
2016-03-29 18:48:45 -0600

    Now can declare items as having been returned.

--- ParkingHelper/app/src/main/java/cmput301w16t01crimsonhorizons/parkinghelper/ReturningStallActivity.java
+++ ParkingHelper/app/src/main/java/cmput301w16t01crimsonhorizons/parkinghelper/ReturningStallActivity.java
@@ -1,7 +1,10 @@
 package cmput301w16t01crimsonhorizons.parkinghelper;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,6 +12,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class ReturningStallActivity extends AppCompatActivity {
     protected Stalls stall;
@@ -22,10 +27,19 @@ public class ReturningStallActivity extends AppCompatActivity {
         setContentView(R.layout.activity_returning_stall);
 
         intent = getIntent();
-        stall = (Stalls)intent.getSerializableExtra("entry");
+        stall = (Stalls)intent.getSerializableExtra("stall");
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        TextView title = (TextView)findViewById(R.id.RetStallNameDisp);
+        TextView description = (TextView)findViewById(R.id.RetStallDescriptionDisp);
+        TextView longitude = (TextView)findViewById(R.id.longitudeRetStallTV);
+        TextView latitude = (TextView)findViewById(R.id.latitudeRetStallTV);
+        title.setText(stall.getOwner());
+        description.setText(stall.getDescription());
+        longitude.setText(stall.getLocation()[0].toString());
+        latitude.setText(stall.getLocation()[1].toString());
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -39,7 +53,25 @@ public class ReturningStallActivity extends AppCompatActivity {
     }
 
     public void returnStall(View view){
+        stall.setStatus("Available");
 
+        ConnectivityManager connectivityManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+        if ((connectivityManager
+                .getNetworkInfo(ConnectivityManager.TYPE_MOBILE) != null && connectivityManager
+                .getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState() == NetworkInfo.State.CONNECTED)
+                || (connectivityManager
+                .getNetworkInfo(ConnectivityManager.TYPE_WIFI) != null && connectivityManager
+                .getNetworkInfo(ConnectivityManager.TYPE_WIFI)
+                .getState() == NetworkInfo.State.CONNECTED)) {
+            Commands command = new EditStallSave(stall);
+            Boolean check = command.execute();
+            if (check) {
+                finish();
+            } else {
+                Toast.makeText(getApplicationContext(), "didn't save", Toast.LENGTH_SHORT).show();
+            }
+        }
+        finish();
     }
 
 }
