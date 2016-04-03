3e710d9c0ea19f2722e131fe5f8e530c1d06ad53
jero1994 <ohman@ualberta.ca>
2016-04-02 19:31:03 -0600

    Delete all bids on a stall upon deleting the stall

--- ParkingHelper/app/src/main/java/cmput301w16t01crimsonhorizons/parkinghelper/DeleteStall.java
+++ ParkingHelper/app/src/main/java/cmput301w16t01crimsonhorizons/parkinghelper/DeleteStall.java
@@ -19,12 +19,19 @@ public class DeleteStall extends Commands {
     public Boolean execute() {
         Boolean check = false;
         ElasticSearchCtr.DeleteStall deleteStall = new ElasticSearchCtr.DeleteStall();
+        ElasticSearchCtr.GetBid getBid = new ElasticSearchCtr.GetBid();
+        ElasticSearchCtr.DeleteBid deleteBid = new ElasticSearchCtr.DeleteBid();
+
+        getBid.execute(new String[] { "StallID", stall.getStallID() });
         deleteStall.execute(stall);
         try {
             check = deleteStall.get();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
+            //Deletes all bids on the stall
+            for (Bid b : getBid.get()) {
+                deleteBid.execute(b);
+                check &= deleteBid.get();
+            }
+        } catch (InterruptedException | ExecutionException e) {
             e.printStackTrace();
         }
         return check;
