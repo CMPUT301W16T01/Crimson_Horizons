46e62cb3ae90507845fbe760fa5436bc821bb645
kliangualberta <kliang@ualberta.ca>
2016-04-04 15:07:07 -0600

    added reveiw displays

--- ParkingHelper/app/src/main/java/cmput301w16t01crimsonhorizons/parkinghelper/ReviewsActivity.java
+++ ParkingHelper/app/src/main/java/cmput301w16t01crimsonhorizons/parkinghelper/ReviewsActivity.java
@@ -1,5 +1,6 @@
 package cmput301w16t01crimsonhorizons.parkinghelper;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -7,19 +8,48 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
 
 /**
  * View for the review activity
  */
 
 public class ReviewsActivity extends AppCompatActivity {
-    ArrayAdapter<String> myAdapter;
+    private ArrayAdapter<String> myAdapter;
+    private ListView reviewsLV;
+    private Stalls stall;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reviews);
-
+        reviewsLV = (ListView)findViewById(R.id.reviewsLVEditStall);
+        Intent intent =getIntent();
+        stall = (Stalls)intent.getSerializableExtra("stall");
+        ElasticSearchCtr.GetReview getReview = new ElasticSearchCtr.GetReview();
+        String[] query = new String[2];
+        query[0]="StallID";
+        query[1]=stall.getStallID();
+        getReview.execute(query);
+        ArrayList<Review>reviewlist = new ArrayList<>();
+        try {
+            reviewlist = getReview.get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        ArrayList<String> review_string = new ArrayList<>();
+        int idx =0;
+        while (idx<reviewlist.size()){
+            review_string.add(reviewlist.get(0).getText().toString());
+            idx = idx+1;
+        }
+        myAdapter = new ArrayAdapter<String>(getApplicationContext(),R.layout.review_layout,review_string);
+        reviewsLV.setAdapter(myAdapter);
     }
 
 }
